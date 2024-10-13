0ff667d4009929b24a4fe8325658ea1df8a5bf4b
Ярослав <jaroslaval@rambler.ru>
2024-10-14 00:50:56 +0300

    Появилось скачивание

--- backend/agent/src/main/java/org/github/babkiniaa/scas/controller/ChallengeController.java
+++ backend/agent/src/main/java/org/github/babkiniaa/scas/controller/ChallengeController.java
@@ -1,10 +1,12 @@
 package org.github.babkiniaa.scas.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.github.babkiniaa.scas.dto.ReportDto;
 import org.github.babkiniaa.scas.entity.Report;
 import org.github.babkiniaa.scas.mapper.ReportMapper;
 import org.github.babkiniaa.scas.service.ReportService;
+import org.github.babkiniaa.scas.textReader.GitStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,6 +25,7 @@ public class ChallengeController {
     private final ReportService reportService;
     private final ExecutorService executorService;;
     private final ReportMapper reportMapper;
+    private final GitStatus gitStatus;
 
 
     @GetMapping("/reports")
@@ -35,4 +38,14 @@ public class ChallengeController {
         return reportService.findById(id);
     }
 
+    @PostMapping("/report/download/url")
+    public Integer downloadUrl(@RequestParam String nameReport, @RequestParam String url) throws GitAPIException {
+        //ТУТ НАДО ПРИБАВИТЬ ID ЕЩЕ ЮЗЕРА, НО Я ЗАБЫЛ КАК
+        ReportDto reportDto = new ReportDto(nameReport);
+        String currentDir = System.getProperty("user.dir") + "/backend/agent/src/main/java/";
+        String currentDirUser = System.getProperty("user.dir") + "/down/" ;
+        gitStatus.cloneRepository(url, currentDirUser);
+        gitStatus.cloneRepository(url, currentDir);
+        return reportService.create(reportDto).getId();
+    }
 }
---
+++ backend/agent/src/main/java/org/github/babkiniaa/scas/textReader/GitStatus.java
@@ -0,0 +1,25 @@
+package org.github.babkiniaa.scas.textReader;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+
+import java.io.File;
+
+public class GitStatus {
+
+    public void cloneRepository(String url, String cloneDirectoryPath) throws GitAPIException {
+        File cloneDirectory = new File(cloneDirectoryPath);
+        try {
+            Git git = Git.cloneRepository()
+                    .setURI(url)
+                    .setDirectory(cloneDirectory)
+                    .call();
+            git.close();
+            git = null;
+        } catch (GitAPIException e) {
+            throw e;
+        }
+
+    }
+
+}
