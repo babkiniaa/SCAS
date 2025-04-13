ea65548f5b7105bd8cf25e61e984619f8f6348a9
KostyaLens <costamanoulov07@gmail.com>
2025-04-13 19:47:22 +0300

    DLM топик

--- backend/agent/src/main/java/org/github/babkiniaa/scas/configuration/KafkaConfiguration.java
+++ backend/agent/src/main/java/org/github/babkiniaa/scas/configuration/KafkaConfiguration.java
@@ -2,10 +2,18 @@ package org.github.babkiniaa.scas.configuration;
 
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.TopicPartition;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -39,5 +47,49 @@ public class KafkaConfiguration {
                 .partitions(1)
                 .replicas(1)
                 .build();
+    }
+
+    @Bean
+    NewTopic createTopicReportDLT() {
+        return TopicBuilder.name("report-create-events-topic.DLT")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    NewTopic createTopicTaskDLT() {
+        return TopicBuilder.name("task-create-events-topic.DLT")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    NewTopic loadTopicTaskDLT() {
+        return TopicBuilder.name("task-load-events-topic.DLT")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
+            ConsumerFactory<String, Object> consumerFactory,
+            @Autowired KafkaTemplate<String, Object> kafkaTemplate) {
+
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(
+                new DeadLetterPublishingRecoverer(
+                        kafkaTemplate,
+                        (record, exception) -> new TopicPartition(record.topic() + ".DLT", record.partition())
+                ),
+                new FixedBackOff(1000L, 3)
+        );
+
+        factory.setCommonErrorHandler(errorHandler);
+        return factory;
     }
 }
